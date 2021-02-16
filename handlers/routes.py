6c08c5b7e7a2f3a9d6e3e330c4400abde46b12c2
ldcarney <liam.carney@noaa.gov>
2021-02-16 16:20:03 -0700

    update

--- handlers/routes.py
+++ handlers/routes.py
@@ -15,7 +15,7 @@ def configure_routes(app):
         data = []
         tags_r = request.args.get("tags", None, str)
         sort = request.args.get("sortBy", "id")
-        dir = request.args.get("direction", "asc")
+        direction = request.args.get("direction", "asc")
         #Send error messages if url is missing tags
         if tags_r is None:
             return jsonify({'error': 'Missing tags parameter'}), 400
@@ -23,7 +23,7 @@ def configure_routes(app):
         if sort not in ["likes", "reads", "popularity", "id"]:
             return jsonify({"error": "Invalid sortBy parameter"}), 400
         #Send error message if url contains invalid direction parameter
-        if dir not in ["asc", "desc"]:
+        if direction not in ["asc", "desc"]:
             return jsonify({"error": "Invalid direction parameter"}), 400
         #Parse Tags
         tags = tags_r.strip().lower().split(",")
@@ -35,7 +35,7 @@ def configure_routes(app):
                 r = requests.get('https://api.hatchways.io/assessment/blog/posts', params=params).json()
                 data += r["posts"]
             #Sort and Direction Parameters applied 
-            if dir == "desc":
+            if direction == "desc":
                 data = sorted(data, key=lambda x: x[sort], reverse=True)
             else:
                 data = sorted(data, key=lambda x: x[sort], reverse=False)
